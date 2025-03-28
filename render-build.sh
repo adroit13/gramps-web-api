6a447dcfb0caf34fb680fcf336a2e3941a8c43ff
adroit13 <de.adroit@gmail.com>
2025-03-28 23:21:31 +0530

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-apt-get update && apt-get install -y \
-    libgirepository1.0-dev \
-    libcairo2-dev \
-    gir1.2-gtk-3.0 \
-    python3-gi \
-    python3-gi-cairo
-pip install -r requirements.txt
